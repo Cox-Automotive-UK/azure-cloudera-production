7596463a8be00de9f3b83266f5ab73d9bc4ba170
caukgit <andy.buckley@manheim.co.uk>
2016-07-06 21:33:27 +0100

    Updated PS

--- ProvisionCloudera.ps1
+++ ProvisionCloudera.ps1
@@ -1,12 +1,10 @@
+﻿Login-AzureRmAccount
 
-
-# Note: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
-$ErrorActionPreference = "stop"
-
-Login-AzureRmAccount
 $subscription = Get-AzureRmSubscription | Out-GridView -Title "Select a Subscription" -PassThru
+
 Select-AzureRmSubscription -SubscriptionId $subscription.SubscriptionId
+
 $ResourceGroupName = (Get-AzureRmResourceGroup).ResourceGroupName | Out-GridView -Title "Select a Resource Group" -PassThru
 
 
-New-AzureRmResourceGroupDeployment -Name ClouderaDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -Verbose
+New-AzureRmResourceGroupDeployment -Name ClouderaProductionDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -Verbose
