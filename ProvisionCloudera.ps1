e43d45ad382dfc1c362b2c14eee0596bb0bb435a
caukgit <andy.buckley@manheim.co.uk>
2017-01-16 15:03:02 +0000

    ui
    op

--- ProvisionCloudera.ps1
+++ ProvisionCloudera.ps1
@@ -7,4 +7,4 @@ Select-AzureRmSubscription -SubscriptionId $subscription.SubscriptionId
 $ResourceGroupName = (Get-AzureRmResourceGroup).ResourceGroupName | Out-GridView -Title "Select a Resource Group" -PassThru
 
 
-New-AzureRmResourceGroupDeployment -Name ClouderaProductionDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -Verbose
+New-AzureRmResourceGroupDeployment -Name ClouderaProductionDeployment -ResourceGroupName $ResourceGroupName -TemplateFile C:\Users\ukadmin\Documents\GitHub\azure-cloudera-production\azuredeploy.json -TemplateParameterFile C:\Users\ukadmin\Documents\GitHub\azure-cloudera-production\azuredeploy.parameters.json -Verbose
